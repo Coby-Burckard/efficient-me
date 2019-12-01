ed673fb20cb72b122db6237013c6f1544a9fc1dd
Coby-Burckard <51294008+Coby-Burckard@users.noreply.github.com>
2019-12-01 15:31:31 -0600

    continued work on index

--- efficient_me/efficient_me_api/urls.py
+++ efficient_me/efficient_me_api/urls.py
@@ -4,14 +4,14 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from django.conf.urls import include
 
 urlpatterns = [
-    path('activities/', views.LCActivity.as_view()),
-    path('activities/<int:pk>', views.RUDActivity.as_view()),
-    path('activitytypes/', views.LCActivityType.as_view()),
-    path('activitytypes/<int:pk>', views.RUDActivityType.as_view()),
-    path('goals/', views.LCGoal.as_view()),
-    path('goals/<int:pk>', views.RUDGoal.as_view()),
-    path('timeallocations/', views.LCTimeAllocation.as_view()),
-    path('timeallocations/<int:pk>', views.RUDTimeAllocation.as_view()),
+    path('api/activities/', views.LCActivity.as_view()),
+    path('api/activities/<int:pk>', views.RUDActivity.as_view()),
+    path('api/activitytypes/', views.LCActivityType.as_view()),
+    path('api/activitytypes/<int:pk>', views.RUDActivityType.as_view()),
+    path('api/goals/', views.LCGoal.as_view()),
+    path('api/goals/<int:pk>', views.RUDGoal.as_view()),
+    path('api/timeallocations/', views.LCTimeAllocation.as_view()),
+    path('api/timeallocations/<int:pk>', views.RUDTimeAllocation.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
